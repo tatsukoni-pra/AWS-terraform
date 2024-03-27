64587ac135d423b479018b97c4e852ff72808be4
小西達大 <konishitatsuhiro@konishitoorudainoMacBook-Pro.local>
2024-03-27 16:13:50 +0900

    feat: 追加

--- import.tf
+++ import.tf
@@ -1,9 +1,34 @@
-import {
-  to = aws_lambda_function.slack_command_test_v1
-  id = "Slack-Command-Test-V1"
-}
+# import {
+#   to = aws_lambda_function.slack_command_test_v1
+#   id = "Slack-Command-Test-V1"
+# }
 
 # import {
 #   to = aws_lambda_permission.slack_command_test_v1
 #   id = "Slack-Command-Test-V1/FunctionURLAllowPublicAccess"
 # }
+
+import {
+  to = aws_cloudwatch_event_rule.import_demo_function
+  id = "default/codepipeline-MyDemo-master-543654-rule"
+}
+
+import {
+  to = aws_lambda_function.import_demo_function
+  id = "import-demo-function"
+}
+
+import {
+  to = aws_lambda_permission.import_demo_function
+  id = "import-demo-function/AWSEvents_codepipeline-MyDemo-master-543654-rule_Id2bbd3deb-6ccc-4a64-bd40-33965589c604"
+}
+
+import {
+  to = aws_lambda_function.import_demo_function_v2
+  id = "import-demo-function-v2"
+}
+
+import {
+  to = aws_lambda_permission.import_demo_function_v2
+  id = "import-demo-function-v2/AWSEvents_codepipeline-MyDemo-master-543654-rule_Ida60db200-8a74-4641-aadf-0e66a1c76504"
+}
--- resource.tf
+++ resource.tf
@@ -31,3 +31,56 @@ resource "aws_lambda_permission" "slack_command_test_v1" {
   principal              = "*"
   function_url_auth_type = "NONE"
 }
+
+data "aws_iam_role" "import_demo_function" {
+  name = "import-demo-function-role-jekuqn1i"
+}
+
+resource "aws_cloudwatch_event_rule" "import_demo_function" {
+  name = "codepipeline-MyDemo-master-543654-rule"
+
+  event_pattern = <<PATTERN
+{
+  "source": ["aws.codecommit"],
+  "detail-type": ["CodeCommit Repository State Change"],
+  "resources": ["arn:aws:codecommit:ap-northeast-1:083636136646:MyDemoRepo"],
+  "detail": {
+    "event": ["referenceCreated", "referenceUpdated"],
+    "referenceType": ["branch"],
+    "referenceName": ["master"]
+  }
+}
+PATTERN
+}
+
+resource "aws_lambda_function" "import_demo_function" {
+  filename      = "./import_demo_function.zip"
+  function_name = "import-demo-function"
+  role          = data.aws_iam_role.import_demo_function.arn
+  handler       = "lambda_function.lambda_handler"
+  runtime       = "python3.12"
+}
+
+resource "aws_lambda_permission" "import_demo_function" {
+  statement_id  = "AWSEvents_codepipeline-MyDemo-master-543654-rule_Id2bbd3deb-6ccc-4a64-bd40-33965589c604"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.import_demo_function.function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.import_demo_function.arn
+}
+
+resource "aws_lambda_function" "import_demo_function_v2" {
+  filename      = "./import_demo_function_v2.zip"
+  function_name = "import-demo-function-v2"
+  role          = data.aws_iam_role.import_demo_function.arn
+  handler       = "lambda_function.lambda_handler"
+  runtime       = "python3.12"
+}
+
+resource "aws_lambda_permission" "import_demo_function_v2" {
+  statement_id  = "AWSEvents_codepipeline-MyDemo-master-543654-rule_Ida60db200-8a74-4641-aadf-0e66a1c76504"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.import_demo_function_v2.arn
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.import_demo_function.arn
+}
