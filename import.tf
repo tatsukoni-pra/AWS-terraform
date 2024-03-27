c0a944cd54ce3b0dd7739f8510332f9858ae6607
小西達大 <konishitatsuhiro@konishitoorudainoMacBook-Pro.local>
2024-03-27 15:00:56 +0900

    feat: 追加

---
+++ import.tf
@@ -0,0 +1,9 @@
+import {
+  to = aws_lambda_function.slack_command_test_v1
+  id = "Slack-Command-Test-V1"
+}
+
+# import {
+#   to = aws_lambda_permission.slack_command_test_v1
+#   id = "Slack-Command-Test-V1/FunctionURLAllowPublicAccess"
+# }
--- resource.tf
+++ resource.tf
@@ -7,3 +7,27 @@ resource "aws_secretsmanager_secret" "test20240226" {
     Name = "test20240226"
   }
 }
+
+data "aws_iam_role" "slack_command_test_v1" {
+  name = "Slack-Command-Test-V1-role-ognoa7lj"
+}
+
+resource "aws_lambda_function" "slack_command_test_v1" {
+  filename      = "./lambda_function_payload.zip"
+  function_name = "Slack-Command-Test-V1"
+  role          = data.aws_iam_role.slack_command_test_v1.arn
+  handler       = "lambda_function.handler"
+  runtime       = "python3.12"
+
+  tags = {
+    Name = "Slack-Command-Test-V1"
+  }
+}
+
+resource "aws_lambda_permission" "slack_command_test_v1" {
+  statement_id           = "FunctionURLAllowPublicAccess"
+  action                 = "lambda:InvokeFunctionUrl"
+  function_name          = aws_lambda_function.slack_command_test_v1.function_name
+  principal              = "*"
+  function_url_auth_type = "NONE"
+}
